a8f273b7c6c05118cfc78cdb26b8b550e79f113a
Mrudula <112971785+Medulla06@users.noreply.github.com>
2024-04-29 08:14:33 +0530

    Update TSP_DFS.py

--- TSP_DFS.py
+++ TSP_DFS.py
@@ -2,32 +2,38 @@ import sys
 
 # The Travelling Salesman Problem using Depth First Search (DFS)
 n = 5
-g = [[0,12,10,19,8],
-        [12,0,3,7,6],
-        [10,3,0,2,20],
-        [19,7,2,0,4],
-        [8,6,20,4,0]]
+graph = [
+    [0, 12, 10, 19, 8],
+    [12, 0, 3, 7, 6],
+    [10, 3, 0, 2, 20],
+    [19, 7, 2, 0, 4],
+    [8, 6, 20, 4, 0]
+]
 
-def tsp(graph,root):
-    stack = [(root,[root])]
-    min_cost = sys.maxsize
+def tsp_dfs(graph, start):
+    visited = [False] * n
+    path = [start]
+    min_cost = [sys.maxsize]
+    iterations = [0]  # Variable to count the iterations
 
-    while stack:
-        current_node, path = stack.pop()
+    def dfs(current_node, depth, cost):
+        iterations[0] += 1  # Increment the iteration count
+        visited[current_node] = True
+
+        if depth == n:
+            cost += graph[current_node][start]  # return to start
+            min_cost[0] = min(min_cost[0], cost)
         
-        if len(path) == n:
-            total_cost = sum(graph[path[i-1]][path[i]] for i in range(1,n))
-            total_cost += graph[path[-1]][path[0]] #return to start
-            min_cost = min(min_cost, total_cost)
-
         else:
             for next_node in range(n):
-                if next_node not in path:
-                    stack.append((next_node, path + [next_node]))
-    
-    return min_cost
+                if not visited[next_node]:
+                    new_cost = cost + graph[current_node][next_node]
+                    dfs(next_node, depth + 1, new_cost)
+        
+        visited[current_node] = False
 
-print(tsp(g,0))
+    dfs(start, 1, 0)
+    print("Number of iterations:", iterations[0])  # Print the number of iterations
+    return min_cost[0]
 
-
-
+print("Minimum cost using DFS:", tsp_dfs(graph, 0))
